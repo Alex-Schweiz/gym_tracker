43bfb5fdfdb4a4abd32e8213e76494854469ca42
Alex Schweiz <askibinskiy@gmail.com>
2017-10-04 23:28:04 +0300

    Sit-ups prepared

--- src/app/admin/situps/situps.component.ts
+++ src/app/admin/situps/situps.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { IMyDpOptions } from 'mydatepicker';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-situps',
@@ -6,10 +11,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./situps.component.css']
 })
 export class SitupsComponent implements OnInit {
+  public modalRef: BsModalRef;
+  public repeats: number;
+  public comments: string;
+  public selectedDate: any;
 
-  constructor() { }
+  situps: FirebaseListObservable<any[]>;
+
+  public myDatePickerOptions: IMyDpOptions = {
+    dateFormat: 'dd.mm.yyyy',
+  };
+
+  constructor(private modalService: BsModalService,
+              db: AngularFireDatabase) {
+    this.situps = db.list('/situps');
+  }
 
   ngOnInit() {
   }
 
+  public openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  addSitup() {
+    const newSitup = new Situp();
+    newSitup.date = this.selectedDate.epoc;
+    newSitup.repeats = this.repeats;
+    newSitup.comments = this.comments;
+    this.modalRef.hide();
+    this.situps.push(newSitup);
+  }
+
+  softDeleteSitup(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  deleteSitup(key: string) {
+    this.situps.remove(key);
+  }
+
 }
+
+class Situp {
+  date: any;
+  repeats: number;
+  comments: string;
+}
+
